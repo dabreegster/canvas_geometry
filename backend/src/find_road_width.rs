274259e2b7850e960441e86637a7645de82265ef
Dustin Carlino <dabreegster@gmail.com>
2023-12-27 10:32:21 +0900

    Figure out the distance of the hit lines

--- backend/src/find_road_width.rs
+++ backend/src/find_road_width.rs
@@ -1,20 +1,22 @@
-use geo::{Coord, Densify, Intersects, Line};
+use geo::{Coord, Densify, EuclideanLength, Intersects, Line, LineIntersection, Polygon};
 use serde::Serialize;
 
 use crate::{MapModel, RoadID};
 
 #[derive(Serialize)]
 pub struct Output {
-    test_points: Vec<TestPoint>,
+    test_lines: Vec<TestLine>,
+    max_left_width: f64,
+    max_right_width: f64,
 }
 
 #[derive(Serialize)]
-pub struct TestPoint {
-    pt: Coord,
-    line_left: Line,
-    left_hits: bool,
-    line_right: Line,
-    right_hits: bool,
+pub struct TestLine {
+    // Right if false
+    left: bool,
+    full_line: Line,
+    // If the line hits something, what's the shortened line and its length?
+    hit: Option<(Line, f64)>,
 }
 
 pub fn find_road_width(map: &MapModel, r: RoadID) -> Output {
@@ -24,31 +26,53 @@ pub fn find_road_width(map: &MapModel, r: RoadID) -> Output {
     // This keeps existing points, which is fine
     let dense_line = map.roads[r.0].linestring.densify(step_size_meters);
 
-    let mut test_points = Vec::new();
+    let mut test_lines = Vec::new();
     // Using lines instead of coords so we can get the angle -- but is this hard to reason about?
     // angle_at_point instead?
     for orig_line in dense_line.lines() {
         // TODO For the last line, use the last point too
         let pt = orig_line.start;
         let angle = orig_line.dy().atan2(orig_line.dx()).to_degrees();
-        let projected_left = project_away(pt, angle - 90.0, project_away_meters);
-        let projected_right = project_away(pt, angle + 90.0, project_away_meters);
-        let line_left = Line::new(pt, projected_left);
-        let line_right = Line::new(pt, projected_right);
 
-        let left_hits = map.buildings.iter().any(|b| b.polygon.intersects(&line_left));
-        let right_hits = map.buildings.iter().any(|b| b.polygon.intersects(&line_right));
+        for (angle_offset, left) in [(-90.0, true), (90.0, false)] {
+            let projected = project_away(pt, angle + angle_offset, project_away_meters);
+            let full_line = Line::new(pt, projected);
 
-        test_points.push(TestPoint {
-            pt,
-            line_left,
-            left_hits,
-            line_right,
-            right_hits,
-        });
+            let mut hit = None;
+            for b in &map.buildings {
+                if b.polygon.intersects(&full_line) {
+                    if let Some(shortened_line) = split_line_by_polygon(full_line, &b.polygon) {
+                        hit = Some((shortened_line, shortened_line.euclidean_length()));
+                        break;
+                    }
+                }
+            }
+
+            test_lines.push(TestLine {
+                left,
+                full_line,
+                hit,
+            });
+        }
     }
 
-    Output { test_points }
+    let mut max_right_width = project_away_meters;
+    let mut max_left_width = project_away_meters;
+    for test in &test_lines {
+        if let Some((_, len)) = test.hit {
+            if test.left {
+                max_left_width = max_left_width.min(len);
+            } else {
+                max_right_width = max_right_width.min(len);
+            }
+        }
+    }
+
+    Output {
+        test_lines,
+        max_left_width,
+        max_right_width,
+    }
 }
 
 fn project_away(pt: Coord, angle_degrees: f64, distance: f64) -> Coord {
@@ -57,4 +81,30 @@ fn project_away(pt: Coord, angle_degrees: f64, distance: f64) -> Coord {
         x: pt.x + distance * cos,
         y: pt.y + distance * sin,
     }
+}
+
+// See also https://github.com/georust/geo/issues/985
+fn split_line_by_polygon(line: Line, polygon: &Polygon) -> Option<Line> {
+    // The input line could intersect the polygon's exterior at several places. Find the hit
+    // closest to line.start.
+    let mut shortest: Option<(Line, f64)> = None;
+    // Ignore polygon holes
+    for polygon_line in polygon.exterior().lines() {
+        if let Some(LineIntersection::SinglePoint { intersection, .. }) =
+            geo::algorithm::line_intersection::line_intersection(line, polygon_line)
+        {
+            // Assume line.start is outside the polygon and we're looking for the place it first
+            // crosses into the polygon
+            let candidate = Line::new(line.start, intersection);
+            let candidate_length = candidate.euclidean_length();
+            if shortest
+                .as_ref()
+                .map(|(_, len)| candidate_length < *len)
+                .unwrap_or(true)
+            {
+                shortest = Some((candidate, candidate_length));
+            }
+        }
+    }
+    shortest.map(|pair| pair.0)
 }
--- web/src/FindRoadWidth.svelte
+++ web/src/FindRoadWidth.svelte
@@ -9,35 +9,43 @@
 <SplitComponent>
   <div slot="sidebar">
     <p>Finding width of this road...</p>
+    <p>Max left: {out.max_left_width.toFixed(2)}</p>
+    <p>Max right: {out.max_right_width.toFixed(2)}</p>
     <div>
       <button on:click={() => mode.set({ mode: "neutral" })}>Back</button>
     </div>
   </div>
   <g slot="map">
-    {#each out.test_points as test}
-      <circle cx={test.pt.x} cy={test.pt.y} r="0.2" />
-      <line class="left" class:hits={test.left_hits} {...lineToSvg(test.line_left)} />
-      <line class="right" class:hits={test.right_hits} {...lineToSvg(test.line_right)} />
+    {#each out.test_lines as test}
+      {@const direction = test.left ? "left" : "right"}
+      {#if test.hit}
+        <line class={`${direction} hits`} {...lineToSvg(test.hit[0])}>
+          <title>Length: {test.hit[1].toFixed(2)}</title>
+        </line>
+      {:else}
+        <line class={direction} {...lineToSvg(test.full_line)} />
+      {/if}
     {/each}
   </g>
 </SplitComponent>
 
 <style>
-  circle {
-    fill: cyan;
+  line {
+    stroke-width: 0.1;
   }
 
   .left {
     stroke: red;
-    stroke-width: 0.1;
   }
 
   .right {
     stroke: blue;
-    stroke-width: 0.1;
   }
 
   .hits {
     stroke-dasharray: 0.5;
+  }
+  .hits:hover {
+    stroke-opacity: 0.5;
   }
 </style>
